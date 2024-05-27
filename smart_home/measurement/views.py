4ef5c3dc34a558dd82d63fee9b38d6259fcef532
Nadya Orekhova <ninilavaet@gmail.com>
2024-05-27 22:09:13 +0300

    add .

---
+++ smart_home/measurement/views.py
@@ -0,0 +1,16 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementPostSerializer, SensorDetailSerializer
+
+class SensorView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorDetailView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+class MeasurementView(CreateAPIView):
+    serializer_class = MeasurementPostSerializer
